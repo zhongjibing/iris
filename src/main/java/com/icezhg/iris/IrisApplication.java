7f1a6448c43b0449017acf5a98d5324afca87d89
zhongjibing <zhongjibing@163.com>
2025-07-09 01:29:08 +0800

    init

---
+++ src/main/java/com/icezhg/iris/IrisApplication.java
@@ -0,0 +1,13 @@
+package com.icezhg.iris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icezhg/iris/IrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icezhg.iris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
